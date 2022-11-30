f4d7cc35e3ec8f0b5b29d507f682624cf93cd9e6
Ajo Varghesegit config --global user.email ajovarghese987@gmail.com <ajovarghese987@gmail.com>
2022-11-30 10:27:20 +0530

    env created

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
+require('dotenv').config(); 
+
 module.exports={
-    serviceID:"VA8d2abb469c76fe5afbc19cca8db38611",
-    accountSID:"AC9c98fdd3a46c04b159bdf4c7a858f3f1",
-    authToken:"6d020912d3b2ebb395fa0a1d266c3ac1"
+    serviceID:process.env.serviceID,
+    accountSID:process.env. accountSID,
+    authToken:process.env.authToken
 }
